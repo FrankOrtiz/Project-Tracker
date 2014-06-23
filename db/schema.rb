8d8efee70f7bfd7a7aa44d59a49e50cf7011d541
Frank Ortiz <ortiz_frank@hotmail.com>
2014-06-23 13:06:17 -0400

    Added users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618211315) do
+ActiveRecord::Schema.define(version: 20140623170550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,14 @@ ActiveRecord::Schema.define(version: 20140618211315) do
   end
 
   add_index "updates", ["project_id"], name: "index_updates_on_project_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
