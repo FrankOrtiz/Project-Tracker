75921566e1b0c84dc52498c02d6083ce0d029f05
Frank Ortiz <ortiz_frank@hotmail.com>
2014-06-23 20:21:10 -0400

    Removed limited title

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 class Project < ActiveRecord::Base
 	has_many :updates
 		validates :who, presence: true
-		validates :name, presence: true,
-						length:(3..50)
+		validates :name, presence: true
 end
