b201bd0f6def6a38615e3a5abf5485ebb16d2f9f
Frank Ortiz <ortiz_frank@hotmail.com>
2014-06-25 11:49:39 -0400

    Now tracks editors of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -72,7 +72,7 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description, :who, :how)
+      params.require(:project).permit(:name, :description, :who, :how, :editor)
     end
     def name
       @project.name
