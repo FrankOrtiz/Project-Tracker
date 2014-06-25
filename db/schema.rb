22f173507e9bd1a4114081431a5670ce52ee6c96
Frank Ortiz <ortiz_frank@hotmail.com>
2014-06-25 15:40:00 -0400

    Projects now properly displayed by latest updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625152047) do
+ActiveRecord::Schema.define(version: 20140625185812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140625152047) do
     t.datetime "updated_at"
     t.string   "created_by"
     t.string   "editor"
+    t.datetime "latest_updated"
   end
 
   create_table "updates", force: true do |t|
