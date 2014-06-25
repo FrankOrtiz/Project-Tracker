2cf7bc6c09e332ddfb09ccd1cade7b1b37ea03de
Frank Ortiz <ortiz_frank@hotmail.com>
2014-06-25 16:40:01 -0400

    Fixed bug: crashing when editing updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
- helper_method :current_user
+ helper_method :current_user, :current_project
 
  private
 
  def current_user 
  	@current_user ||= User.find_by(id: session[:user_id]) 
  end 
-
+ 	
  def require_logged_in 
 	return true if current_user 
 	redirect_to root_path 
