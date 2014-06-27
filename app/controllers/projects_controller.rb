b92c79e3070d180fd3ed2031f5cda25b0a7a2c48
Frank Ortiz <ortiz_frank@hotmail.com>
2014-06-27 10:57:50 -0400

    Fixed updates bug where updates of projects existed after project has been deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -58,6 +58,10 @@ class ProjectsController < ApplicationController
   # DELETE /projects/1.json
   def destroy
     @project.destroy
+    updates = Update.where(project_id: @project.id)
+    updates.each do |update|
+      update.destroy
+    end
     respond_to do |format|
       format.html { redirect_to projects_url, notice: 'Project was removed.' }
       format.json { head :no_content }
